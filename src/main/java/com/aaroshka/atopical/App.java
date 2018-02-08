919fa8a5c8328ca3b94dc16da75031e8967eb261
catalystcf <arzona@gmail.com>
2018-02-07 23:05:00 -0500

    added plugin information to make executable jar for heroku

--- src/main/java/com/aaroshka/atopical/App.java
+++ src/main/java/com/aaroshka/atopical/App.java
@@ -2,14 +2,24 @@ package com.aaroshka.atopical;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Hello world!
  *
  */
+@Controller
 @SpringBootApplication
 public class App 
 {
+	
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 	
     public static void main( String[] args )
     {
